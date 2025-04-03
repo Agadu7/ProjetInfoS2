f07f9fc32b28d154772cc5a00360d3288a597fae
MaelDerrien <derrienmael1@gmail.com>
2025-04-03 09:37:49 +0200

    Test main

--- projetinfo/src/main/java/Main.java
+++ projetinfo/src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Gaetan mon bébou à la crême");
+        Atelier A = new Atelier( 1);
+        Machine m1 = new Machine("1","machine_croissant" , 0, 0, "viennoiserie", 100);
+        Machine m2 = new Machine("2","machine_painchocolat" , 1, 0, "viennoiserie", 100);
+        Machine m3 = new Machine("3","machine_beignet" , 0, 1, "viennoiserie", 100);
+        Machine m4 = new Machine("4","machine_muffin" , 1, 1, "viennoiserie", 100);
+        Personne p1 = new Personne(null,"Dupont", "Gérard",1);
     }
 }
