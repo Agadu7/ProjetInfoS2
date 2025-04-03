c95378902b81a6144175864e788a618f0735e989
MaelDerrien <derrienmael1@gmail.com>
2025-04-03 09:54:56 +0200

    Cheffe et Abd-El

--- projetinfo/src/main/java/Main.java
+++ projetinfo/src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Machine m2 = new Machine("2","machine_painchocolat" , 1, 0, "viennoiserie", 100);
         Machine m3 = new Machine("3","machine_beignet" , 0, 1, "viennoiserie", 100);
         Machine m4 = new Machine("4","machine_muffin" , 1, 1, "viennoiserie", 100);
-        Personne p1 = new Personne(null,"Dupont", "Gérard",1);
+        Personne p1 = new Personne(null,"Dadouche", "Abd-Elraouf",1);
+        ChefAtelier Cheffe = new ChefAtelier("Duboeuf Eleonore", A);
     }
 }
