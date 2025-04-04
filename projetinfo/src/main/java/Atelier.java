82b9b02bfab3e7bfedd89cb96628a5f5577666ef
Agadu7 <couzigou.gaetan@gmail.com>
2025-04-04 15:38:08 +0200

    Modification Atelier

--- projetinfo/src/main/java/Atelier.java
+++ projetinfo/src/main/java/Atelier.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class Atelier {
     private int codeAtelier;
     private ArrayList<Personne> listePersonne;
+    private ArrayList<Machine> listeMachine;
+    private ArrayList<Poste> listePoste;
 
     public int getCodeAtelier() {
         return codeAtelier;
@@ -19,8 +21,27 @@ public class Atelier {
         this.listePersonne = listePersonne;
     }
 
-    public Atelier (int codeAtelier,ArrayList<Personne> listePersonne){
+    public Atelier (int codeAtelier,ArrayList<Personne> listePersonne,ArrayList<Machine> listeMachine,ArrayList<Poste> listePoste){
         this.codeAtelier=codeAtelier;
         this.listePersonne=listePersonne;
+        this.listePoste=listePoste;
+        this.listeMachine=listeMachine;
     }
+
+    public ArrayList<Machine> getListeMachine() {
+        return listeMachine;
+    }
+
+    public void setListeMachine(ArrayList<Machine> listeMachine) {
+        this.listeMachine = listeMachine;
+    }
+
+    public ArrayList<Poste> getListePoste() {
+        return listePoste;
+    }
+
+    public void setListePoste(ArrayList<Poste> listePoste) {
+        this.listePoste = listePoste;
+    }
+    
 }
